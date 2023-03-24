b7b1fe059654545a9723f46213696957f6625a36
Manumdt <mmateosdt@gmail.com>
2023-03-24 13:22:47 +0100

    Commit nuevo

---
+++ Prueba01/src/Prueba01/prueba.java
@@ -0,0 +1,11 @@
+
+package Prueba01;
+
+public class prueba {
+	
+	public static void main(String args[]) {
+		System.out.println("Hola");
+		System.out.println("Que tal?");
+		System.out.println("Bien y tu?");
+	}
+}
