ee61bad1be3f2d9bc84b513aaebdf233523f9d6c
Manumdt <mmateosdt@gmail.com>
2023-03-27 09:21:11 +0200

    Prueba SSH 2

--- Prueba_git/src/prueba_git/prueba_git.java
+++ Prueba_git/src/prueba_git/prueba_git.java
@@ -4,7 +4,7 @@ public class prueba_git {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba");
-		System.out.println("Prueba SSH");
+		System.out.println("Prueba SSH 2");
 
 	}
 
