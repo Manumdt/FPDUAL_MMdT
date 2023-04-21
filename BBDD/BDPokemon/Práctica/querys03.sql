9a37534055ddcb9703604b3eb1bc48480d5d9dd1
Manumdt <mmateosdt@gmail.com>
2023-04-21 10:54:47 +0200

    docs(Base de Datos): :memo: Enunciados querys 02 y 03

---
+++ BBDD/BDPokemon/Práctica/querys03.sql
@@ -0,0 +1,6 @@
+USE dbpokemon;
+
+-- 1. Mostrar todos los pokemon que evolucionan por piedra. 
+-- 2. Mostrar todos los pokemons que no evolucionan (80 Registros)
+-- 3. Mostrar la cantidad de los pokemon de cada tipo. Hacer una vista de ello.
+-- 4. Mostrar todos los nombres que aprende Charmander por MO
