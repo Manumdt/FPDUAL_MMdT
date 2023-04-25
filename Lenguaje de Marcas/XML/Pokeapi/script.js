39a99dfae96b21719d3700aac6bd50dea67a4c7e
Manumdt <mmateosdt@gmail.com>
2023-04-25 09:11:26 +0200

    fix(Lenguaje de Marcas): :rotating_light:

--- Lenguaje de Marcas/XML/Pokeapi/script.js
+++ Lenguaje de Marcas/XML/Pokeapi/script.js
@@ -1,33 +1,33 @@
-const pokemonContainer= document.querySelector('.pokemon-container');
-const anterior= document.querySelector('#anterior');
-const siguiente= document.querySelector('#siguiente');
-const oculto= document.querySelector('.oculto');
+const pokemonContainer = document.querySelector('.pokemon-container');
+const anterior = document.querySelector('#anterior');
+const siguiente = document.querySelector('#siguiente');
+const oculto = document.querySelector('.oculto');
 
-let offset=1;
-let limit= 19;
+let offset = 1;
+let limit = 23;
 
-anterior.addEventListener('click', ()=>{
-    if(offset !=1){
-        offset-=20;
+anterior.addEventListener('click', () => {
+    if (offset != 1) {
+        offset -= 24;
         removePokemons(pokemonContainer);
         fetchAllPokemons(offset, limit);
     }
 });
 
-siguiente.addEventListener('click', ()=>{
-    if(offset <=1008){
-        offset+=20;
+siguiente.addEventListener('click', () => {
+    if (offset <= 1008) {
+        offset += 24;
         removePokemons(pokemonContainer);
         fetchAllPokemons(offset, limit);
     }
 });
 
-function fetchPokemon(id){
+function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then((res)=> res.json())
-    .then((data)=> {
-        crearPokemon(data);
-    });
+        .then((res) => res.json())
+        .then((data) => {
+            crearPokemon(data);
+        });
 }
 
 /*function fetchOculto(id){
@@ -38,14 +38,17 @@ function fetchPokemon(id){
     });
 }*/
 
-function fetchAllPokemons(offset, limit){
-    for(let i=offset; i<=offset+limit; i++){
+function fetchAllPokemons(offset, limit) {
+    for (let i = offset; i <= offset + limit; i++) {
         fetchPokemon(i);
     }
 }
 
-function crearPokemon(pokemon){
-    const carta= document.createElement('div');
+function crearPokemon(pokemon) {
+
+    let url = pokemon.species.url;
+
+    const carta = document.createElement('div');
     carta.classList.add('pokemon');
 
     // carta.addEventListener('click', ()=>{
@@ -54,10 +57,39 @@ function crearPokemon(pokemon){
     //     crearOculto(pokemon.id);
     // });
 
-    $(".pokemon").click(function() {
+    const desc = pokemon.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text; /** Filtramos el contenido por idioma y sólo cogemos la descripción el español ('es') */
+    const pokemon_oculto = document.createElement('div');
+    const descripcion = document.createElement('p')
+    pokemon_oculto.classList.add('pokemon_oculto');
+    descripcion.classList.add("descripcion");
+    descripcion.textContent = desc;
+    //descripcion.style.display = "none"; /* Dejamos la descripción oculta hasta que hagamos click */
+
+    pokemon_oculto.appendChild(descripcion);
+    oculto.appendChild(pokemon_oculto);
+
+
+    $(".pokemon").click(function () {
         $(".oculto").show(100);
         $(".fondo_oculto").show(200);
-        crearOculto(pokemon.id);
+        // crearOculto(pokemon.id);
+
+        // fetch(url)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         const desc = data.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text; /** Filtramos el contenido por idioma y sólo cogemos la descripción el español ('es') */
+        //         const pokemon_oculto = document.createElement('div');
+        //         pokemon_oculto.classList.add('pokemon_oculto');
+        //         descripcion.classList.add("descripcion");
+        //         descripcion.textContent = desc;
+        //         //descripcion.style.display = "none"; /* Dejamos la descripción oculta hasta que hagamos click */
+
+        //         pokemon_oculto.appendChild(descripcion);
+        //         oculto.appendChild(pokemon_oculto);
+        //     }); 
+        
+
+
 
         /*
         const pokemonOculto=document.createElement('div');
@@ -80,39 +112,39 @@ function crearPokemon(pokemon){
         oculto.appendChild(sprite_number);
     
         oculto.appendChild(pokemonOculto);*/
-        });
+    });
 
-    $(".oculto").click(function() {
-         $(".oculto").hide(500);
-         $(".fondo_oculto").hide(500);
-         removePokemons(pokemon_oculto);
-        });
+    $(".oculto").click(function () {
+        $(".oculto").hide(500);
+        $(".fondo_oculto").hide(500);
+        //  removePokemons(pokemon_oculto);
+    });
 
-    const spriteContainer=document.createElement('div');
+    const spriteContainer = document.createElement('div');
     spriteContainer.classList.add('sprite');
 
-    const imagen=document.createElement('img');
-    imagen.src= pokemon.sprites.front_default;
-    
-    const number= document.createElement('p');
-    number.textContent= `#${pokemon.id.toString().padStart(3, 0)}`;
-    
-    const name= document.createElement('p');
-    name.classList.add('name');
-    name.textContent=pokemon.name;
+    const imagen = document.createElement('img');
+    imagen.src = pokemon.sprites.front_default;
 
-    const tipos= document.createElement('div');
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name;
+
+    const tipos = document.createElement('div');
     tipos.classList.add('tipos');
-    const tipo1= document.createElement('p');
-    const tipo2= document.createElement('p');
-    if(pokemon.types.length==1){
-        tipo1.textContent=  pokemon.types[0].type.name;
-        tipo1.classList.add('tipo1',pokemon.types[0].type.name);
-    }else if(pokemon.types.length==2){
-        tipo1.textContent= pokemon.types[0].type.name;
-        tipo1.classList.add('tipo1',pokemon.types[0].type.name);
-        tipo2.textContent= pokemon.types[1].type.name;
-        tipo2.classList.add('tipo2',pokemon.types[1].type.name);
+    const tipo1 = document.createElement('p');
+    const tipo2 = document.createElement('p');
+    if (pokemon.types.length == 1) {
+        tipo1.textContent = pokemon.types[0].type.name;
+        tipo1.classList.add('tipo1', pokemon.types[0].type.name);
+    } else if (pokemon.types.length == 2) {
+        tipo1.textContent = pokemon.types[0].type.name;
+        tipo1.classList.add('tipo1', pokemon.types[0].type.name);
+        tipo2.textContent = pokemon.types[1].type.name;
+        tipo2.classList.add('tipo2', pokemon.types[1].type.name);
     }
 
     spriteContainer.appendChild(imagen);
@@ -127,40 +159,40 @@ function crearPokemon(pokemon){
     pokemonContainer.appendChild(carta);
 }
 
-function  crearOculto(id, pokemon_oculto){
+// function  crearOculto(id, pokemon_oculto){
 
-    // fetchOculto();
-        
-    id -= 1;
+//     // fetchOculto();
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then((res) => res.json())
-        .then((data) => {
-            crearOculto(id, data);
-        });
-        
-    const div_oculto = document.createElement('div');
-    div_oculto.classList.add('pokemon_oculto');
+//     id -= 1;
 
-    const sprite_number = document.createElement('div');
-    sprite_number.classList.add('sprite_number');
+//         fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             crearOculto(id, data);
+//         });
 
-    const sprite = document.createElement('img');
-    sprite.classList.add('img_oculto');
-    sprite.src = pokemon_oculto.sprites.front_default;
-    
-    const number = document.createElement('p');
-    number.classList.add('number_oculto');
-    number.textContent = `#${pokemon_oculto.id.toString().padStart(3, 0)}`;
-    
-    sprite_number.appendChild(sprite);
-    sprite_number.appendChild(number);
-    
-    oculto.appendChild(sprite_number);
-}
+//     const div_oculto = document.createElement('div');
+//     div_oculto.classList.add('pokemon_oculto');
 
-function removePokemons(parent){
-    while(parent.firstChild){
+//     const sprite_number = document.createElement('div');
+//     sprite_number.classList.add('sprite_number');
+
+//     const sprite = document.createElement('img');
+//     sprite.classList.add('img_oculto');
+//     sprite.src = pokemon_oculto.sprites.front_default;
+
+//     const number = document.createElement('p');
+//     number.classList.add('number_oculto');
+//     number.textContent = `#${pokemon_oculto.id.toString().padStart(3, 0)}`;
+
+//     sprite_number.appendChild(sprite);
+//     sprite_number.appendChild(number);
+
+//     oculto.appendChild(sprite_number);
+// }
+
+function removePokemons(parent) {
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
