6eb2286dabab9255b8e534d67134e9da8bfcd78e
Manumdt <mmateosdt@gmail.com>
2023-04-28 10:19:44 +0200

    feat(Base de Datos): :sparkles: ProyectoFinalCursoSQLAvanzado Login

---
+++ BBDD/CursoAvanzado/ProyectoFinal/Insert.sql
@@ -0,0 +1,6 @@
+USE login;
+
+INSERT INTO usuarios (Usuario,Contrasena) VALUES('Kiri503','Kiri12345'),
+('Manumdt','Manu310700');
+
+SELECT * FROM usuarios;
---
+++ BBDD/CursoAvanzado/ProyectoFinal/logueo.php
@@ -0,0 +1,25 @@
+<?php
+
+include('db.php');
+
+$usuario= $_POST['usuario'];
+$contrasena= $_POST['contrasena'];
+
+session_start();
+$_SESSION['usuario']=$usuario;
+
+$logueo= "SELECT * 
+          FROM usuarios
+          WHERE Usuario='$usuario' AND Contrasena= '$contrasena'";
+
+
+$validacion= mysqli_query($conexion, $logueo);
+$filas= mysqli_fetch_row($validacion);
+
+if($filas!=null){
+    header('location:inicio.php');
+}else{
+    echo "Ha ocurrido un error al loguearte, vuelve a intentarlo más tarde";
+}
+mysqli_free_result($validacion);
+?>
