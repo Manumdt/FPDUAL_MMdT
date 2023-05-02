de813a75bb5fbffe66311834c604b707a9330e17
Manumdt <mmateosdt@gmail.com>
2023-05-02 10:53:46 +0200

    docs(Base de Datos): :memo: Enunciados querys 02 y 03

---
+++ BBDD/BDPokemon/Práctica/querys02.sql
@@ -0,0 +1,7 @@
+USE dbpokemon;
+
+-- 1. Mostrar el nombre y tipo del ataque con más potencia:
+-- 2. Muestra el numero pokedex, nombre, ataque y velocidad de todos los pokemons con ataque >50 y velocidad <75 ordenados por nombre.
+-- 3. Mostrar el número de movimientos de cada tipo.
+-- 4. Mostrar todos los movimientos que puedan envenenar.
+-- 5. Mostrar todos los movimientos que aprende pikachu.
