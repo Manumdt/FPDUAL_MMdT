23c6b559f4408e3d13107ab07baaf24c88da60d5
Manumdt <mmateosdt@gmail.com>
2023-05-12 11:37:03 +0200

    refactor(Programación): :recycle: Eliminar import que no se usan

--- Programación/Eclipse-Workspace/Proyecto05_PracticoPokemon/src/util/Main.java
+++ Programación/Eclipse-Workspace/Proyecto05_PracticoPokemon/src/util/Main.java
@@ -17,9 +17,7 @@ import java.net.URL;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
 
 public class Main {
 
